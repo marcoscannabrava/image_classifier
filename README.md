286ffbaf4f63832b21da304072ae78b5be513ceb
Marcos Cannabrava <mpcannabrava@gmail.com>
2023-06-21 15:57:58 -0700

    update

---
+++ README.md
@@ -0,0 +1,32 @@
+# README
+
+Simple Image Classifier Model.
+
+- Trained on 1 CPU, 1 GPU (Cuda and MacOS) and multiple GPUs (Cuda).
+- Using Pytorch and Pytorch Lightning
+
+
+# Learnings
+
+- GPUs are slower than CPUs unless there's enough data for parallel threads to make a difference.
+
+
+# Experiments
+Training time with a batch size of 4 on single CPU: ~55s
+Training time with a batch size of 4 on single GPU: ~1m54s
+Accuracy ~ 54%
+
+Training time with a batch size of 1000 on single CPU: ~49s
+Training time with a batch size of 1000 on single GPU: ~28s
+Accuracy < 15%
+*A couple category had high accuracy (>75%) while all the others had 0%.
+
+Training time with a batch size of 4 and wider hidden layers (20 nodes between conv layers) on single CPU: ~1m3s
+Training time with a batch size of 4 and wider hidden layers (20 nodes between conv layers) on single GPU: ~1m53s
+Accuracy ~ 58%
+
+# Questions
+- Can pin_memory improve speed of the GPU? hypothesis: is the memory transfer causing a slowdown?
+
+# References
+[Training a Classifier — PyTorch Tutorials 2.0.1+cu117 documentation](https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html)
--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import torch
-import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
 import torchvision
 import torchvision.transforms as transforms
 
@@ -17,10 +18,10 @@ transform = transforms.Compose(
 batch_size = 4
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2) # type: ignore
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2) # type: ignore
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2) # type: ignore
+testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2) # type: ignore
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -54,3 +55,61 @@ net = Net()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+# Training
+def train(device=None):
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = (data[0].to(device), data[1].to(device)) if device else data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
+
+
+def test(device=None):
+    correct = 0
+    total = 0
+
+    # prepare to count predictions for each class
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = (data[0].to(device), data[1].to(device)) if device else data
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            for label, prediction in zip(labels, predicted):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
