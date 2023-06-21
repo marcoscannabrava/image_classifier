e33e44bafe569085ebf3830c18506c1d50f033a9
Marcos Cannabrava <mpcannabrava@gmail.com>
2023-06-21 11:49:24 -0700

    pure pytorch on single GPU

---
+++ utils.py
@@ -0,0 +1,19 @@
+from model import trainloader, torchvision, classes, batch_size
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imshow(img):
+    img = img / 2 + 0.5     # denormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
