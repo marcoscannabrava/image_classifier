c093ca0f2c437a4bfa67e941063e1b00b195ec49
Marcos Cannabrava <mpcannabrava@gmail.com>
2023-06-21 21:02:19 -0700

    add lightning and refactor

---
+++ data.py
@@ -0,0 +1,22 @@
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+
+# Data loading and preprocessing
+
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(), 
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ]
+)
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2) # type: ignore
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2) # type: ignore
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
